291ff8a3c17479046923b4dafa791f8581218dfe
Dominic Martin Cruz <>
2024-02-13 14:50:58 +0800

    removed mongo, routers, users model

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import 'dotenv/config'
-import mongoose from 'mongoose'
-import router from './router';
+
+import { authentication, random } from './helpers';
 
 const app = express();
+const PORT = process.env.PORT
  
 app.use(cors({
   credentials: true,
@@ -16,17 +17,55 @@ app.use(cors({
 
 app.use(compression());
 app.use(cookieParser());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const server = http.createServer(app)
 
-server.listen(8080, () => {
-  console.log('Server running on port 8080');
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 })
 
-mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGO_URL);
-mongoose.connection.on('error', (error: Error) => console.log(error));
-mongoose.connection.on('connected', () => console.log('mongoose connected'));
+const users: any[] = []
 
-app.use('/', router());
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post('/users', (req, res) => {
+  try {
+    const { email, password, firstName, lastName } = req.body;
+
+    // check if req is complete
+    if(!email || !password || !firstName || !lastName) {
+      return res.status(400).send('missing credentials')
+    }
+
+    // check if user exists
+    const existingUser = users.filter(user => user.email === email).length > 0
+    if(existingUser) {
+      return res.status(400).send('user already exists')
+    }
+    
+    // "create" user
+    const salt = random();
+
+    const user = {
+      email,
+      firstName,
+      lastName,
+      authentication: {
+        salt,
+        password: authentication(salt, password)
+      }
+    }
+    users.push(user)
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(5)
+    return res.status(400).send(error)
+  }
+});

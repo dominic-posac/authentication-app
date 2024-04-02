13a002ce06dc489a48ce455874af8d9435721abb
Dominic Martin Cruz <>
2024-04-02 14:47:37 +0800

    update to oop

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import http from 'http';
+import express, { Application, Request, Response } from 'express';
+import http, { Server as HTTPServer } from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
@@ -10,109 +10,137 @@ import { loginFieldErrors, registerFieldErrors } from './utils/constants/FieldEr
 import { sendRegistrationEmail } from './utils/mailer';
 import { getMailData } from './utils/constants/MailData';
 
-const app = express();
-const PORT = process.env.PORT
+class Authentication {
+  salt: string;
+  password: string;
 
-app.use(compression());
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-const server = http.createServer(app)
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-})
-
-const users: UserInterface[] = [
-  // test user
-  {
-    "email": "dominic@positiveaction.ph",
-    "firstName": "Dominic",
-    "lastName": "Cruz",
-    "authentication": {
-        "salt": "QnYrg66yi9p/j8diTFE+Jl3RLjJQvnJ4hZ+WX4pQoJo6fzUuxWZZdNyM9aNeDXeNTQJgEcA7ulSVFmTmdAxv0sWYxxEKnZ2GSx4o/n247cJj+a9U8J8srUPwOao8FJhEc+W46Ap2oa/LNrsIcnQL29hP+umZBqaGXl65ze+15W4=",
-        "password": "f22f89265c8b9a2bffe00bd88d30c2992212bdbbf87125a1daae723ba601a4d7"
-    }
+  constructor(salt: string, password: string) {
+    this.salt = salt;
+    this.password = password;
   }
-]
+}
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+class User {
+  email: string;
+  firstName: string;
+  lastName: string;
+  authentication: Authentication;
 
-app.post('/register', async (req, res) => {
-  const { email, firstName, lastName, password } = req.body
-  const fieldsFromReq: FieldsInterface = {
-    email,
-    firstName,
-    lastName,
-    password
+  constructor(fields: UserInterface) {
+    this.email = fields.email;
+    this.firstName = fields.firstName;
+    this.lastName = fields.lastName;
+    this.authentication = new Authentication(fields.authentication.salt, fields.authentication.password);
+  }
+}
+
+class Server {
+  private app: Application;
+  private server: HTTPServer;
+  private PORT: string | number;
+  private users: UserInterface[];
+
+  constructor() {
+    this.app = express();
+    this.PORT = process.env.PORT || 3000;
+    this.users = [];
+    this.setupServer();
   }
 
-  try {
-    // check if req is complete
-    const missingCreds = checkMissingFields(fieldsFromReq, registerFieldErrors)
-    if(missingCreds.length > 0) {
-      return res.status(400).send(missingCreds)
-    }
+  private setupServer(): void {
+    this.app.use(compression());
+    this.app.use(cookieParser());
+    this.app.use(bodyParser.urlencoded({
+      extended: true
+    }));
 
-    if(checkIfUserExists(users, email)) {
-      return res.status(400).send('User already exists.')
-    }
-    
-    // "create" user
-    const salt = random();
-    const user = {
+    this.server = http.createServer(this.app);
+
+    this.server.listen(this.PORT, () => {
+      console.log(`Server running on port ${this.PORT}`);
+    });
+
+    this.setupRoutes();
+  }
+
+  private setupRoutes(): void {
+    this.app.get("/users", this.getUsers.bind(this));
+    this.app.post('/register', this.registerUser.bind(this));
+    this.app.post('/login', this.loginUser.bind(this));
+  }
+
+  private getUsers(req: Request, res: Response): void {
+    res.send(this.users);
+  }
+
+  private async registerUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { email, firstName, lastName, password } = req.body;
+    const fieldsFromReq: FieldsInterface = {
       email,
       firstName,
       lastName,
-      authentication: {
-        salt,
-        password: authentication(salt, password)
+      password
+    };
+
+    try {
+      const missingCreds = checkMissingFields(fieldsFromReq, registerFieldErrors);
+      if (missingCreds.length > 0) {
+        return res.status(400).send(missingCreds);
       }
-    }
-    users.push(user)
 
-    // if(user creation is success)
-    const registerEmail = await sendRegistrationEmail(getMailData({ email, firstName, lastName }))
-    console.log(registerEmail)
-
-    return res.status(200).send('You have successfully registered!').end();
-  } catch (error) {
-    return res.status(400).send(error)
-  }
-});
-
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body
-  const fieldsFromReq: FieldsInterface = {
-    email,
-    password
-  }
-
-  try {
-    // check if req is complete
-    const missingCreds = checkMissingFields(fieldsFromReq, loginFieldErrors)
-    if(missingCreds.length > 0) {
-      return res.status(400).send(missingCreds)
-    }
-    // check if user exists
-    const user = checkIfUserExists(users, email)
-    if(user) {
-      // compare password hash
-      const passwordFromLoginHash = authentication(user.authentication.salt, password);
-      if(user.authentication.password !== passwordFromLoginHash) {
-        return res.status(403).send('Incorrect password!')
+      if (checkIfUserExists(this.users, email)) {
+        return res.status(400).send('User already exists.');
       }
-      // if password match, login success, send token?
-      return res.status(200).send('Login Successful!').end();
+
+      const salt = random();
+      const fields = {
+        email,
+        firstName,
+        lastName,
+        authentication: {
+          salt,
+          password: authentication(salt, password)
+        }
+      };
+      const newUser = new User(fields);
+      this.users.push(newUser);
+
+      const registerEmail = await sendRegistrationEmail(getMailData({ email, firstName, lastName }));
+      console.log(registerEmail);
+
+      return res.status(200).send('You have successfully registered!').end();
+    } catch (error) {
+      return res.status(400).send(error);
     }
-    else {
-      return res.status(400).send('User does not exist!')
-    }
-  } catch (error) {
-    return res.status(400).send(error)
   }
-});
+
+  private async loginUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>> {
+    const { email, password } = req.body;
+    const fieldsFromReq: FieldsInterface = {
+      email,
+      password
+    };
+
+    try {
+      const missingCreds = checkMissingFields(fieldsFromReq, loginFieldErrors);
+      if (missingCreds.length > 0) {
+        return res.status(400).send(missingCreds);
+      }
+
+      const user = checkIfUserExists(this.users, email);
+      if (user) {
+        const passwordFromLoginHash = authentication(user.authentication.salt, password);
+        if (user.authentication.password !== passwordFromLoginHash) {
+            return res.status(403).send('Incorrect password!');
+        }
+        return res.status(200).send('Login Successful!').end();
+      } else {
+        return res.status(400).send('User does not exist!');
+      }
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+}
+
+const server = new Server();

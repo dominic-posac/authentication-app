7c5bea28d6a6a5630786f4d1b7d0ef38bbee2992
Dominic Martin Cruz <>
2024-04-05 11:50:00 +0800

    cleanup tab

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -49,29 +49,29 @@ export const registerUser = async (req: Request, res: Response): Promise<Respons
 }
 
 export const loginUser = async (req: Request, res: Response): Promise<Response<any, Record<string, any>>> => {
-    const { email, password } = req.body;
-    const fieldsFromReq: FieldsInterface = {
-      email,
-      password
-    };
+  const { email, password } = req.body;
+  const fieldsFromReq: FieldsInterface = {
+    email,
+    password
+  };
 
-    try {
-      const missingCreds = checkMissingFields(fieldsFromReq, loginFieldErrors);
-      if (missingCreds.length > 0) {
-        return res.status(400).send(missingCreds);
-      }
-
-    const user = UserEntity.checkIfUserExists(email);
-    if (user) {
-        const passwordFromLoginHash = new Authentication(user.authentication.salt, password);
-        if (user.authentication.password !== passwordFromLoginHash.password) {
-          return res.status(403).send('Incorrect password!');
-        }
-        return res.status(200).send('Login Successful!').end();
-      } else {
-        return res.status(400).send('User does not exist!');
-      }
-    } catch (error) {
-      return res.status(400).send(error);
+  try {
+    const missingCreds = checkMissingFields(fieldsFromReq, loginFieldErrors);
+    if (missingCreds.length > 0) {
+      return res.status(400).send(missingCreds);
     }
+
+  const user = UserEntity.checkIfUserExists(email);
+  if (user) {
+      const passwordFromLoginHash = new Authentication(user.authentication.salt, password);
+      if (user.authentication.password !== passwordFromLoginHash.password) {
+        return res.status(403).send('Incorrect password!');
+      }
+      return res.status(200).send('Login Successful!').end();
+    } else {
+      return res.status(400).send('User does not exist!');
+    }
+  } catch (error) {
+    return res.status(400).send(error);
   }
+}

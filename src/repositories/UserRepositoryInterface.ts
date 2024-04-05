cb7d5364b880ed3f362e02f64d47bd5489724137
Dominic Martin Cruz <>
2024-04-05 15:01:16 +0800

    renamed GenericRepositoryInterface and RepositoryA

--- src/repositories/UserRepositoryInterface.ts
+++ src/repositories/UserRepositoryInterface.ts
@@ -1,6 +1,6 @@
 import { UserEntity } from "../classes/UserEntity";
 
-export interface GenericRepositoryInterface {
+export interface UserRepositoryInterface {
   getUsers(): Promise<UserEntity[]>;
   findUser(email: string): Promise<UserEntity>;
   addUser(newUser: UserEntity): Promise<void>;

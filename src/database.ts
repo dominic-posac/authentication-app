752e7b8afe1540cf8bb3bb97eeaedf5a96b71e90
Dominic Martin Cruz <>
2024-04-15 19:28:36 +0800

    updated docker compose  and database.ts

--- src/database.ts
+++ src/database.ts
@@ -2,8 +2,11 @@ import mysql from "mysql2"
 import 'dotenv/config';
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  // host:  process.env.DB_HOST, // use this if without docker
+  host:  "mysqldb",
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 }).promise()
+
+pool.query("CREATE TABLE `authentication-app`.`users` (`id` INT NOT NULL AUTO_INCREMENT,`email` VARCHAR(100) NOT NULL,`firstName` VARCHAR(100) NOT NULL,`lastName` VARCHAR(100) NOT NULL,`password` VARCHAR(100) NOT NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE)")

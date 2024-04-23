2e7f1f324b7a84d1053322e99f06f89a5856b9fd
Dominic Martin Cruz <>
2024-04-23 16:58:18 +0800

    cleanup

--- src/classes/UserEntity.ts
+++ src/classes/UserEntity.ts
@@ -1,24 +1,6 @@
 import { UserInterface } from "../types/UserInterface.types";
-// import { TypeormUserEntity } from "./TypeormUserEntity";
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm"
 import 'dotenv/config';
-
-// export class UserEntity implements UserInterface {
-//   email: string;
-//   firstName: string;
-//   lastName: string;
-//   password: string;
-
-//   constructor(fields: UserInterface) {
-//     Object.assign(this, fields);
-//   }
-
-//   static async createUser(fields: UserInterface) {
-//     const newUser = new TypeormUserEntity;
-//     Object.assign(newUser, fields);
-//     return newUser
-//   }
-// }
 
 @Entity({ name: "typeorm_users" })
 export class UserEntity implements UserInterface {
